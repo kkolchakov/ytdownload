16b0faa44f25808531dc69dbede1e09cd033781c
kkolchakov <66190090+kkolchakov@users.noreply.github.com>
2023-05-01 22:34:26 -0500

    Update ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -1,1 +1,32 @@
 
+import subprocess
+#import settings
+
+def video_download(url):
+
+    subprocess.run(['yt-dlp', url])
+    print('Finished downloading your video!')
+
+def audio_download(url):
+
+    subprocess.run(['yt-dlp','-x', '--audio-format', 'mp3', url])
+    print("Finished downloading your audio file!")
+
+
+if __name__ == '__main__':
+    l = True
+    url = input('Enter url:  ')
+    while l:
+
+        choice = input('Enter a-audio/v-video/c-cancel:   ')
+
+        if choice == 'a':
+            audio_download(url)
+        elif choice == 'v':
+            video_download(url)
+
+        elif choice =='c':
+            break
+        else:
+            print('Enter valid choice!')
+
